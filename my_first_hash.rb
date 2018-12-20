d103a88cbac59fb92882089b442d967e390110a3
Joe Ganobsik <jganobsik@gmail.com>
2018-12-20 16:29:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+your_hash = {'team' => 'Columbus Blue Jackets', 'favorite city' => 'London'}
   
 end
 
